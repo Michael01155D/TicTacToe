74f0da51de87acdb418d0fbb10def2e5f6abb20a
Michael Mroz <michaelmroz01111@gmail.com>
2024-01-03 20:16:07 -0400

    add option to run game from cli using args as gridsize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
+
 public class Main {
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe(31);
-        game.play();
+
+        if (args.length == 0){
+            TicTacToe game = new TicTacToe(3);
+            game.play();
+        }
+
+        if (args.length > 0){
+            try {
+                int gridSize = Integer.parseInt(args[0]);
+                TicTacToe game = new TicTacToe(gridSize);
+                game.play();
+            } catch (Exception e){
+                System.err.println("Argument" + args[0] + " must be an integer between 3 and 31.");
+                System.exit(1);
+            }
+        }
     }
+
 
 }
